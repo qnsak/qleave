bed8d59c2f7251fb1d9b63bf08a0ca3fbffefc73
qns <sak6289@gmail.com>
2023-07-12 11:17:09 +0800

    refactor : 移除開發使用的程式

--- forestage/src/components/Header.vue
+++ forestage/src/components/Header.vue
@@ -20,32 +20,11 @@
 
     const getUserInfo = async () => {
         let getUserInformation = await api.user.getUserInformation();
-        console.log(getUserInformation.data)
         setUserInfo(getUserInformation.data)
-    }
-
-    const leavespanst = reactive([])
-    const getLeavespanst = async () => {
-        let getLeavespanstResult = await api.leave.getLeaveComingspanst()
-        leavespanst.length = 0
-        Object.assign(leavespanst, getLeavespanstResult.data);
-    }
-
-    const getDate = (input) => {
-        let date = new Date(input);
-
-        const year = date.getFullYear()
-        const month = String(date.getMonth() + 1).padStart(2, '0')
-        const day = String(date.getDate()).padStart(2, '0')
-        const hours = String(date.getHours()).padStart(2, '0')
-        const minute = String(date.getMinutes()).padStart(2, '0')
-
-        return `${year}-${month}-${day} ${hours}:${minute}`
     }
 
     onMounted(() => {
         getUserInfo();
-        getLeavespanst()
     })
     
 </script>
