f4a4e72854a856d6eefad04cf31d899520d05ca3
qns <sak6289@gmail.com>
2023-07-12 11:21:57 +0800

    fix : paging 功能無法正常顯示

--- forestage/src/views/leave/Approve.vue
+++ forestage/src/views/leave/Approve.vue
@@ -24,13 +24,23 @@
       let getLeaveListResult = await api.leave.getApproveLeaveList(page)
       leaveList.length = 0;
       Object.assign(leaveList, getLeaveListResult.data);
-      console.log( getLeaveListResult)
+      let prevUrl = 0;
+      let nextUrl = 0;
+      if (getLeaveListResult.links) {
+        if (getLeaveListResult.links.prev) {
+          prevUrl = parseInt(getLeaveListResult.links.prev, 10);
+        }
+        if (getLeaveListResult.links.next) {
+          nextUrl = parseInt(getLeaveListResult.links.next, 10);
+        }
+      }
+
       let pageInfo = {
-        'from': getLeaveListResult.meta.from,
-        'to': getLeaveListResult.meta.to,
-        'total': getLeaveListResult.meta.total,
-        'prev': getLeaveListResult.links.prev,
-        'next': getLeaveListResult.links.next,
+        'from': getLeaveListResult.meta.from || 0,
+        'to': getLeaveListResult.meta.to || 0,
+        'total': getLeaveListResult.meta.total ||0,
+        'prev': prevUrl,
+        'next': nextUrl,
       }
       setPagingInfo(pageInfo);
   }
@@ -171,15 +181,14 @@
           @click="getLeaveList(pagingInfo.prev)"
           v-if="pagingInfo.from > 1"
         >
-          <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>
-          Prev
+          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>
         </button>
         <button
           class="inline-flex items-center px-4 py-2 m-2 border-2 border-slate-700 text-sm font-medium"
           @click="getLeaveList(pagingInfo.next)"  
           v-if="pagingInfo.next !== 0"
         >
-          <svg aria-hidden="true" class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
       </div>
     </div>
